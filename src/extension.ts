8380e5b44b3b637aa2c45e35fbd5dab4f6808e49
even.chang <even1101u@gmail.com>
2025-03-25 08:43:59 +0000

    chore: 注釋掉未實作的規則初始化命令

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,18 @@
 import * as vscode from 'vscode';
 // back to start
-import { initializeVarRule } from './rules/var/varRule';
-import { initializeReturnRule } from './rules/return/returnRule';
-import { initializeAwaitRule } from './rules/await/awaitRule';
-import { initializeIfRule } from './rules/if/ifRule';
-import { initializeTaskRule } from './rules/task/taskRule';
-import { initializeAsyncRule } from './rules/async/asyncRule';
+// import { initializeVarRule } from './rules/var/varRule';
+// import { initializeReturnRule } from './rules/return/returnRule';
+// import { initializeAwaitRule } from './rules/await/awaitRule';
+// import { initializeIfRule } from './rules/if/ifRule';
+// import { initializeTaskRule } from './rules/task/taskRule';
+// import { initializeAsyncRule } from './rules/async/asyncRule';
 // current line 1 postion to end
 import { initializeVarALineRule } from './rules/var/varALineRule';
 import { initializeReturnALineRule } from './rules/return/returnALineRule';
-import { initializeAwaitALineRule } from './rules/await/awaitALineRule';
+// import { initializeAwaitALineRule } from './rules/await/awaitALineRule';
 import { initializeIfALineRule } from './rules/if/ifALineRule';
-import { initializeTaskALineRule } from './rules/task/taskALineRule';
-import { initializeAsyncALineRule } from './rules/async/asyncALineRule';
+// import { initializeTaskALineRule } from './rules/task/taskALineRule';
+// import { initializeAsyncALineRule } from './rules/async/asyncALineRule';
 // Quick Tools
 import { registerNGUID } from './features/nguid';
 import { registerNDate } from './features/ndate';
@@ -24,20 +24,20 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		// init rule
-		initializeVarRule(context);
-		initializeReturnRule(context);
-		initializeAwaitRule(context);
-		initializeIfRule(context);
-		initializeAsyncRule(context);
-		initializeTaskRule(context);
+		// Not yet implemented command
+		// initializeVarRule(context);
+		// initializeReturnRule(context);
+		// initializeAwaitRule(context);
+		// initializeIfRule(context);
+		// initializeAsyncRule(context);
+		// initializeTaskRule(context);
 
 		initializeVarALineRule(context);
 		initializeReturnALineRule(context);
-		initializeAwaitALineRule(context);
+		// initializeAwaitALineRule(context);
 		initializeIfALineRule(context);
-		initializeAsyncALineRule(context);
-		initializeTaskALineRule(context);
+		// initializeAsyncALineRule(context);
+		// initializeTaskALineRule(context);
 
 		vscode.window.showInformationMessage('Like Rider Initialize success');
 	});
